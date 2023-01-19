d9f6ea18c1b2f231d05df7b3a3b78b3571608f04
devenuub <devenuub@gmail.com>
2023-01-18 22:46:16 -0500

    BIG FIXES 1/18/2023

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-//display mobile menu//
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
