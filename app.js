9680999fc4bc8739d59460de344ebdbb7bde7d19
devenuub <devenuub@gmail.com>
2023-07-31 15:26:25 -0400

    TRANSPARENCY

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// Add event listener to menu links
+menuLinks.addEventListener('click', function() {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+});
